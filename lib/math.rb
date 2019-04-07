2271f33cdc1aad83b55941dc7c99b96ea82ff78a
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-07 02:59:40 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,22 +3,23 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  90-67
+  num1-num2
 end
 
 def division(num1, num2)
-  99/11
+  num2/num1
 end
 
 def multiplication(num1, num2)
-  6*5
+  num1*num2
 end
 
 def modulo(num1, num2)
-  67%2
+  num1%num2
 end
 
 def square_root(num)
+  
 end
 
 def order_of_operation(num1, num2, num3, num4)
